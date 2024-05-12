1fc8eafae15be43b8c9d6ad279b70fa7281e9b64
Jouueur <brossetmarius@gmail.com>
2024-05-12 14:32:39 +0200

    test fx project

---
+++ src/main/java/com/example/cybookfx/User.java
@@ -0,0 +1,36 @@
+package com.example.cybookfx;
+
+
+import java.util.*;
+public class User {
+    static int idCount = 0;
+    private String firstName;
+    private String lastName;
+    private final int id;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = ++idCount;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
