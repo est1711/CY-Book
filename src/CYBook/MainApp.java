4bf66184af97c852d553e11f187cf891ef7b851e
alexy <telpioupiou@gmail.com>
2024-05-13 13:21:09 +0200

    nouveau MainApp

--- src/CYBook/MainApp.java
+++ src/CYBook/MainApp.java
@@ -1,17 +1,44 @@
 package CYBook;
 import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class MainApp {
 
         public static void main(String[] args) {
+
             Scanner sc = new Scanner(System.in);
             int choice = 0;
+
+            try {
+                Connection connection = DriverManager.getConnection(
+                        "jdbc:mysql://127.0.0.1:3306/login_schema",
+                        "root",
+                        "book"
+                );
+
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
+
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("user_name"));
+                    System.out.println(resultSet.getString("user_password"));
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
             while(true){
                 System.out.println("Choose the option of your choice by typing the associated number");
                 System.out.println("(1) --> Book search ");
                 System.out.println("(2) --> Create a customer account");
                 System.out.println("(3) --> Display customer information");
                 System.out.println("(4) --> View all overdue books");
+                System.out.println("\n(0) --> Quit");
                 System.out.print("\nYour choice : ");
 
                 try {
@@ -24,6 +51,9 @@ public class MainApp {
                 }
 
                 switch (choice) {
+                    case 0:
+                        System.out.println("Exiting the application. Goodbye!");
+                        System.exit(0);
                     case 1:
                         bookSearch();
                         break;
@@ -36,13 +66,13 @@ public class MainApp {
                     case 4:
                         viewOverdueBooks();
                         break;
-                    case 5:
-                        System.out.println("Exiting the application. Goodbye!");
-                        System.exit(0);
                     default:
                         System.out.println("Invalid choice. Please select a number between 1 and 5.");
                 }
             }
+
+
+
 
         }
 
